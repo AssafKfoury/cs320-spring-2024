4a0667e0af9d6148e785b529578bf69b30bf0d92
Assaf Kfoury <kfoury@bu.edu>
2024-04-09 23:42:23 -0400

    aaaa

--- assigns/assign00/assign00_03.ml
+++ assigns/assign00/assign00_03.ml
@@ -1,2 +1,12 @@
 (* THIS IS A TEST AND ONLY A TEST AND NOTHING BUT A TEST *)
 let square (x : int) : int = x * x 
+let id = fun x -> x 
+let sq = fun x -> x * x
+let rec myst : int -> int = fun x -> myst x (* always diverging *)
+let proj_1 = fun x y -> x ;;
+let proj_2 = fun x y -> y ;;
+
+(* values to try:
+   10.0 ** 10.0    
+
+*)
