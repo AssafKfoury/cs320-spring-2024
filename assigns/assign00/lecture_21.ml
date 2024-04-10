a7def34a77352d2b0dbf37c98b9682b882d4c813
Assaf Kfoury <kfoury@bu.edu>
2024-04-10 19:10:04 -0400

    aaaa

--- assigns/assign00/lecture_21.ml
+++ assigns/assign00/lecture_21.ml
@@ -1,15 +1,23 @@
 (* Material for CS 320, lecture 21, April 11, 2024 *)
 
 (*
-CALL-BY-VALUE
+CALL-BY-VALUE (CBV)
 The arguments of a function are evaluated and
 then their results are passed to the function.
 *)
 
 (*
-CALL-BY-NAME
+CALL-BY-NAME (CBN)
 The arguments of a function are substituted directly 
 in the function’s body and evaluated only when necessary.
+*)
+
+(*
+THUNK
+A thunk is a function of type ()-> T where T is the type of the body
+of the function. Thunks are used to delay evaluation, i.e., if an
+argument of a function is of thunk type, it is not evaluated until
+needed, in a call-by-name style. Thunks simulate CBN in a CBV language.
 *)
 
 (* the doubling function *)
@@ -44,10 +52,9 @@ let proj_1 = fun x y -> y ;;
 let proj_2 = fun x y -> x ;;
 
 (* 
-   
 Try the following expressions after loading this script with:
     # use "lecture_21.ml"
-Suspend utop with "Ctrl z" if stuck in infinite loop.
+If stuck in an infinite loop, suspend utop with "Ctrl z" 
 
 id myst
 myst 3
@@ -57,12 +64,18 @@ proj_1 (myst 3) ;;
 proj_1 myst 10               -- int = 10
 proj_1 (myst 3) 10           -- diverges
 proj_1 (fun z -> myst 3) 10  -- int = 10 
-
 *)
 
-let arg () = print_endline "Function evaluated.";;
+let foo () = print_endline "Argument evaluated.";;
 
-let cbv_fn f =
+let cbv_fn f =          (* Note: f is a dummy argument *)
     print_endline "=> Not using the argument, evaluated anyway." ;;
-let cbn_fn f =
+let cbn_fn f =          (* Note: f is a dummy argument *)
     print_endline "=> Not using the argument, not evaluated."  ;;
+let bar _ = 
+    print_endline "CALL-BY-VALUE:";
+        cbv_fn (foo ());
+    print_endline "CALL-BY-NAME:";
+        cbn_fn (fun () -> foo ())     
+        (* evaluation of foo () is suspendent by wrapping it in a lambda *)
+      ;;
